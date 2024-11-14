f94fec81358760dc5e7a70d7a980f99bf57fac06
Prasanna V <you@example.com>
2024-11-14 13:33:48 +0530

    used the concept arrayy of objects

--- phone.cpp
+++ phone.cpp
@@ -12,11 +12,43 @@ private:
     string PhoneName;
     int IMEInumber;
     string brand;
+    bool isAvailable;
 
 public:
-    void check() {
-        cout << "Welcome to this Phone Showroom!!" << endl;
-        cout << "I am using this :)" << endl;
+    Phone() : PhoneName(""), IMEInumber(0), brand(""), isAvailable(false) {}
+
+    Phone(string name, int num, string brand, bool status)
+        : PhoneName(name), IMEInumber(num), brand(brand), isAvailable(status) {}
+
+    void uploadPhone(string name, int number, string Brand, bool status) {
+        PhoneName = name;
+        IMEInumber = number;
+        brand = Brand;
+        isAvailable = status;
+        cout << "Phone details are updated here:\n";
+        displayPhones();
+    }
+
+    void displayPhones() const {
+        cout << "Phone name: " << PhoneName << "\n"
+             << "IMEI Number: " << IMEInumber << "\n"
+             << "Brand: " << brand << "\n"
+             << "availability status: " << (isAvailable ? "Not available" : "Available") << "\n";
+    }
+
+    bool isPresent() const {
+        return !isAvailable && IMEInumber > 0;
+    }
+
+    bool orderPhone() {
+        if (isPresent()) {
+            isAvailable = true;
+            cout << "Phone successfully purchased :- " << PhoneName << "\n";
+            return true;
+        } else {
+            cout << "The model " << PhoneName << " is not available.\n";
+            return false;
+        }
     }
 };
 
@@ -24,6 +56,7 @@ class User {
 private:
     string name;
     int id;
+
 public:
     void buyPhone(int id) {
         cout << "I am interested in buying phone from your shop." << endl;
@@ -32,11 +65,16 @@ public:
 };
 
 int main() {
-    Phone *Samsung = new Phone;
-    User *Prasanna = new User;
-    Samsung->check();
-    Prasanna->buyPhone(123);
-    delete Samsung;
-    delete Prasanna;
-    return 0;
+    Phone phone[3] = {
+        Phone("Galaxy s24 ultra", 101, "Samsung", false),
+        Phone("Iphone 16 pro", 209, "Apple", false),
+        Phone("Note 13 pro", 130, "Redmi", true)
+    };
+    for (int i = 0; i < 3; ++i) {
+        phone[i].displayPhones();
+    }
+    for (int i = 0; i < 3; ++i) {
+        cout << "Wishing to buy " << phone[i].isPresent() << endl;
+        phone[i].orderPhone();
+    }
 }
